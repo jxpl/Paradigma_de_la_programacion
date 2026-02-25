68c3307b15f34366ffac5b2e425149bff767fe81
jxpl <juanpa050909@gmail.com>
2026-02-25 15:58:16 -0500

    primmer s

---
+++ Programacion_secuencial.js
@@ -0,0 +1,17 @@
+let nombre = "pablo";
+let edad = 10;
+let apellido = "Rivera";
+let numero = 30;
+
+function sumar(a,b) {
+      return a + b ;
+}
+function restar(a,b) {
+    let operacion = a-b;
+    return operacion;
+}
+
+console.log ("Hola, " + nombre + " con apellido " + apellido + " y edad de " + edad);
+console.log ("El resultado de la suma es: " + sumar(numero,edad));
+let Resultado_resta = restar(4,edad);
+console.log("El resultado de la resta es: " + Resultado_resta)
